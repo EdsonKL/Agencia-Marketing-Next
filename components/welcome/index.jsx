2f53d39d70bf2bf746f2e13d0bce17e891bb1e21
EdsonKL <zedsonkillerz@gmail.com>
2024-03-30 00:28:30 -0300

    Ajustes de Header, Welcome e pastas de estilo

--- components/welcome/index.jsx
+++ components/welcome/index.jsx
@@ -1,17 +1,19 @@
 import { Button } from '../button'
+import Image from 'next/image'
+import leads from '../../public/images/leads.svg'
 import styles from './welcome.module.scss'
 
 export const Welcome = () => {
     return(
-        <div>
+        <div className={styles.container}>
             <div className={styles.text}>
                 <h1>Melhor agência de marketing do bairro</h1>
                 <p>Somos uma agência de performance digital, aceleramos vendas e aquisição de leads para grandes marcas.</p>
                 <Button title="Aumentar Vendas" kind="secondary"/>
             </div>
 
-            <div className={styles.image}>
-
+            <div className={styles.DivImage}>
+                <Image className={styles.image} src={leads} alt='Banner Leads'/>
             </div>
         </div>
     )
